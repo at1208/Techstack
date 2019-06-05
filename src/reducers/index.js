01b88953c46e0a503b7a25347573737a1cd2a9ba
at1208 <amantiwari12081999@gmail.com>
2019-06-05 16:14:35 +0530

    thumnail added

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,7 +2,7 @@ import { combineReducers } from 'redux';
 
 const TechReducer = () => {
   return [
-    { id: 0, name: 'Html', imgURL:'macfold.png', url:'macfold.png'},
+    { id: 0, name: 'Html', imgURL:'0.png', url:'macfold.png'},
     { id: 1,name: 'Css',imgURL:'02.png', url:'macfold.png'},
     { id: 2,name: 'Javascript',imgURL:'03.png', url:'macfold.png'},
     { id: 3,name: 'Node.js',imgURL:'04.png', url:'macfold.png'},
@@ -21,7 +21,7 @@ const TechReducer = () => {
     { id: 16,name: 'C++',imgURL:'17.png', url:'macfold.png'},
     { id: 17,name: 'C#',imgURL:'18.png', url:'macfold.png'},
     { id: 18,name: 'Git',imgURL:'19.png', url:'macfold.png'},
-    { id: 19,name: 'Bootstrap',imgURL:'macfold.png', url:'macfold.png'},
+    { id: 19,name: 'Bootstrap',imgURL:'20.png', url:'macfold.png'},
     { id: 20,name: 'Latex',imgURL:'macfold.png', url:'macfold.png'},
     { id: 21,name: 'Matlab',imgURL:'macfold.png', url:'macfold.png'},
     { id: 22,name: 'Tensorflow',imgURL:'23.png', url:'macfold.png'},
@@ -30,12 +30,13 @@ const TechReducer = () => {
     { id: 25,name: 'NLP',imgURL:'macfold.png', url:'macfold.png'},
     { id: 26,name: 'Deep Learning',imgURL:'macfold.png', url:'macfold.png'},
     { id: 27,name: 'Web2py',imgURL:'28.png', url:'macfold.png'},
-    { id: 28,name: 'OCaml',imgURL:'macfold.png', url:'macfold.png'},
+    { id: 28,name: 'OCaml',imgURL:'29.png', url:'macfold.png'},
     { id: 29,name: 'Prolog',imgURL:'30.png', url:'macfold.png'},
     { id: 30,name: 'Amazon DynamoDB',imgURL:'31.png', url:'macfold.png'},
     { id: 31,name: 'Data Stucture',imgURL:'macfold.png', url:'macfold.png'},
     { id: 32,name: 'Algorithm',imgURL:'macfold.png', url:'macfold.png'},
-    { id: 33,name: 'PWA', imgURL:'34.png', url:'macfold.png'}
+    { id: 33,name: 'PWA', imgURL:'34.png', url:'macfold.png'},
+    { id: 33,name: 'Go', imgURL:'35.png', url:'macfold.png'}
 
     ]
 }
